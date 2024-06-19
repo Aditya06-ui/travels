93be9f554c552053fc3c094319fafc4349f8f3c9
Aditya06-ui <adityaguptakumar9876@gmail.com>
2024-06-19 14:35:26 +0530

    fixed url

--- databases/database.js
+++ databases/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const db = () => {
     mongoose.connect(process.env.mongoose_url)
-    .then(console.log("database connected"))
+    .then((c) => console.log(`database connected to ${c.connection.host}`))
     .catch((err) => console.log("failed to connect" + err))
 }
 
