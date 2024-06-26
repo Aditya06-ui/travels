97a5ba84e90d665ecaaa2ca203bf987f7965f63f
Aditya06-ui <adityaguptakumar9876@gmail.com>
2024-06-26 16:46:39 +0530

    improve port number

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors")
 app.use(cookieparser())
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.Frontend_url],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     headers: ["Content-Type", "Authorization"]
